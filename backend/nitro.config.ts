0078359ac896b321b72670da6f1b033c37300172
吳元皓 <hpware3@gmail.com>
2025-01-28 12:41:23 +0800

    a

--- backend/nitro.config.ts
+++ backend/nitro.config.ts
@@ -1,5 +1,8 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2025-01-28"
+  compatibilityDate: "2025-01-28",
+  experimental: {
+    database: true,
+  },
 });
